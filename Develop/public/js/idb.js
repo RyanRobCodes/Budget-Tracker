aed8f03b06397ffde05a5b8b90f0601dffef4ab1
Ryanrobcodes <ryan.robinsonlb@gmail.com>
2022-04-09 18:18:06 -0400

    add service worker

---
+++ Develop/public/js/idb.js
@@ -0,0 +1,15 @@
+let db;
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_budget', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
