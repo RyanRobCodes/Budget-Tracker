fc187fa7fe5f14a2755f95b1bca2702db3ff8e11
Ryanrobcodes <ryan.robinsonlb@gmail.com>
2022-04-10 22:36:04 -0400

    second deployment

--- Develop/server.js
+++ Develop/server.js
@@ -16,10 +16,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  });
 
 // routes
 app.use(require("./routes/api.js"));
